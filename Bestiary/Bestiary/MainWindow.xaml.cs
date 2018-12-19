f38db78f10a256d471f182ecb6112d3ab087b1db
Bernardo Arana <bernardo.giron99@gmail.com>
2018-12-19 16:28:13 -0600

    MainWindow is almost done.

--- Bestiary/Bestiary/MainWindow.xaml.cs
+++ Bestiary/Bestiary/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,28 @@ namespace Bestiary
     /// </summary>
     public partial class MainWindow : Window
     {
+        int i = 0;
         public MainWindow()
         {
+           
+            Timer timer = new Timer(TimerCallBack,null,0,1000);
             InitializeComponent();
+            Hi.Play();
+        
+           
+
+                
+        }
+        private void TimerCallBack(object o)
+        {
+          
+        }
+
+        private void Image_AnimationCompleted(object sender, RoutedEventArgs e)
+        {
+            img_Logo.Visibility = Visibility.Visible;
+           
+
         }
     }
 }
